a0d0b873ae857d7c7852c82363dafc90a2697d48
Fenn <autumne@nevada.unr.edu>
2024-05-06 14:28:22 -0700

    Title add

--- HeaderHavok/Assets/Scripts/livechange.cs
+++ HeaderHavok/Assets/Scripts/livechange.cs
@@ -1,14 +1,14 @@
-using UnityEngine.UI; // Don't forget this line
-public class Script : MonoBehaviour
-{
-   public Text scoreText;
-   public float counter;
-    public Text liveText;
+// using UnityEngine.UI; // Don't forget this line
+// public class Script : MonoBehaviour
+// {
+//    public Text scoreText;
+//    public float counter;
+//     public Text liveText;
  
-   public void Update()
-   {
-        scoreText.text =  "Score: " + counter.ToString();
-        liveText.text =  "Lives: " + counter.ToString();
-        counter++;
-   }
-}
+//    public void Update()
+//    {
+//         scoreText.text =  "Score: " + counter.ToString();
+//         liveText.text =  "Lives: " + counter.ToString();
+//         counter++;
+//    }
+// }
